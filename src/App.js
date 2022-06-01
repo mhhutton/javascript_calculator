122050bfb472ba2e40ac5aa69b8dc578d7e8b958
mhhutton <mhhutton@gmail.com>
2022-05-31 19:22:40 -0600

    Backspace function works.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
-  const [display, setDisplay] = useState(0);
+  const [display, setDisplay] = useState('0');
 
-  const handleDisplay = (input) => {
+  const handleDisplay = (calcInput) => {
+    //IF display is equal to 0 then input may be able to replace 0
+    if (display === '0') {
+      //IF calcInput is only numbers 1-9 and period then calcInput will replace 0
+      if (calcInput.match(/([1-9.])/)) {
+        const myprom = new Promise(() => {
+          setDisplay(calcInput);
+        })
+        myprom.then(()=> {})
 
+      }
+    }
+    //IF input is not equal to zero then input will add onto display
+    else if (display !== '0') {
+      setDisplay(display + calcInput);
+      //console.log(display)
+    }
+  }
+  const handleBackspace = () => {
+    if (display !== '0') {
+      if (display.length > 1) {
+        setDisplay(display.slice(0, display.length-1));
+      } else if (display.length === 1) {
+          setDisplay('0');
+      }
+    }
   }
 
   return (
@@ -25,31 +49,53 @@ function App() {
         <Row>
           <Col
             className="calc-btn" id="clear"
-            xs={4}
-            onClick={() => {setDisplay(0)}}
+            xs={3}
+            onClick={() => {setDisplay('0')}}
           >
             Clear
           </Col>
           <Col
+            className="calc-btn" id="clear"
+            xs={3}
+            onClick={() => { handleBackspace() }}
+          >
+            Back
+          </Col>
+          <Col
             className="calc-btn"
             id="parens"
-            xs={4}
+            xs={3}
           >
             ( )
           </Col>
           <Col
             className="calc-btn"
             id="divide"
-            xs={4}
+            xs={3}
           >
             /
           </Col>
         </Row>
         <Row>
-          <Col className="calc-btn" id="seven" xs={3}>7</Col>
+          <Col
+            className="calc-btn"
+            id="seven"
+            xs={3}
+            value="7"
+            onClick={() => {handleDisplay('7')}}
+          >
+            7
+        </Col>
           <Col className="calc-btn" id="eight" xs={3}>8</Col>
           <Col className="calc-btn" id="nine" xs={3}>9</Col>
-          <Col className="calc-btn" id="multiply" xs={3}>x</Col>
+          <Col
+            className="calc-btn"
+            id="multiply"
+            xs={3}
+            onClick={() => {handleDisplay('x')}}
+          >
+            x
+          </Col>
         </Row>
         <Row>
           <Col className="calc-btn" id="four" xs={3}>4</Col>
@@ -60,13 +106,34 @@ function App() {
         <Row>
           <Col className="calc-btn" id="one" xs={3}>1</Col>
           <Col className="calc-btn" id="two" xs={3}>2</Col>
-          <Col className="calc-btn" id="three" xs={3}>3</Col>
+          <Col
+            className="calc-btn"
+            id="three"
+            xs={3}
+            onClick={() => {handleDisplay('3')}}
+          >
+              3
+          </Col>
           <Col className="calc-btn" id="add" xs={3}>+</Col>
         </Row>
         <Row>
           <Col className="calc-btn" id="pos-neg" xs={3}>+/-</Col>
-          <Col className="calc-btn" id="zero" xs={3}>0</Col>
-          <Col className="calc-btn" id="decimal" xs={3}>.</Col>
+          <Col
+            className="calc-btn"
+            id="zero"
+            xs={3}
+            onClick={() => {handleDisplay('0')}}
+          >
+            0
+          </Col>
+          <Col
+            className="calc-btn"
+            id="decimal"
+            xs={3}
+            onClick={() => {handleDisplay('.')}}
+          >
+            .
+          </Col>
           <Col className="calc-btn" id="equals" xs={3}>=</Col>
         </Row>
       </Container>
